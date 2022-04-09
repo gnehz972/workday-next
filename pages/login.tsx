7bc33d6e8a28579f2d913c144757edd8f0ea66a4
gnehz972 <zoz20042008@gmail.com>
2022-04-09 23:14:43 +0800

    add login success redirect msg

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,7 @@
 import { FC, useEffect, useState } from "react";
 import {
   Box,
+  CircularProgress,
   FormControl,
   IconButton,
   InputAdornment,
@@ -16,69 +17,83 @@ import { useRouter } from "next/router";
 
 const Login: FC = () => {
   const [showPwd, setShowPwd] = useState(false);
+  const [showRedirect, setShowRedirect] = useState(false);
   const [pwd, setPwd] = useState("");
   const [username, setUsername] = useState("");
   const { login, loginSuccess, loading } = useLogin();
-  console.log("loginSuccess=", loginSuccess);
   const router = useRouter();
 
   useEffect(() => {
     if (loginSuccess) {
-      router.replace("/");
+      setShowRedirect(true);
+      router.replace("/").then((it) => setShowRedirect(false));
     }
   }, [loginSuccess]);
 
   return (
-    <Box display="flex" alignItems="center" justifyContent="center">
-      <Stack
-        spacing={4}
-        alignItems={"center"}
-        alignSelf={"center"}
-        sx={{ width: "40ch" }}
-      >
-        <Typography variant={"body1"} sx={{ marginTop: 15, fontSize: 20 }}>
-          Workday
-        </Typography>
-        <FormControl sx={{ width: "100%", marginTop: 15 }} variant="outlined">
-          <InputLabel>用户</InputLabel>
-          <OutlinedInput
-            type={"text"}
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-            label="Username"
-          />
-        </FormControl>
-        <FormControl sx={{ width: "100%" }} variant="outlined">
-          <InputLabel>密码</InputLabel>
-          <OutlinedInput
-            type={showPwd ? "text" : "password"}
-            value={pwd}
-            onChange={(event) => setPwd(event.target.value)}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={() => setShowPwd(!showPwd)}
-                  edge="end"
-                >
-                  {showPwd ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
-          />
-        </FormControl>
-        <LoadingButton
-          sx={{ width: "100%", height: 40 }}
-          variant="contained"
-          loading={loading}
-          endIcon={<LoginOutlined />}
-          loadingPosition="end"
-          onClick={async () => await login(username, pwd)}
+    <Box
+      display="flex"
+      flexDirection={"column"}
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Typography variant={"body1"} sx={{ marginTop: 10, fontSize: 20 }}>
+        Workday
+      </Typography>
+      {showRedirect && (
+        <Stack direction={"row"} alignItems={"center"} marginTop={2}>
+          <CircularProgress size={20} />
+          <Typography>登陆成功，跳转首页中...</Typography>
+        </Stack>
+      )}
+      {!showRedirect && (
+        <Stack
+          spacing={4}
+          alignItems={"center"}
+          alignSelf={"center"}
+          sx={{ width: "40ch" }}
         >
-          登录
-        </LoadingButton>
-      </Stack>
+          <FormControl sx={{ width: "100%", marginTop: 10 }} variant="outlined">
+            <InputLabel>用户</InputLabel>
+            <OutlinedInput
+              type={"text"}
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+              label="Username"
+            />
+          </FormControl>
+          <FormControl sx={{ width: "100%" }} variant="outlined">
+            <InputLabel>密码</InputLabel>
+            <OutlinedInput
+              type={showPwd ? "text" : "password"}
+              value={pwd}
+              onChange={(event) => setPwd(event.target.value)}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={() => setShowPwd(!showPwd)}
+                    edge="end"
+                  >
+                    {showPwd ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+          <LoadingButton
+            sx={{ width: "100%", height: 40 }}
+            variant="contained"
+            loading={loading}
+            endIcon={<LoginOutlined />}
+            loadingPosition="end"
+            onClick={async () => await login(username, pwd)}
+          >
+            登录
+          </LoadingButton>
+        </Stack>
+      )}
     </Box>
   );
 };
